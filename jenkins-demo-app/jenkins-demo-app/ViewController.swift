58fef337b334636448408de8ad5c9cbed88d0f04
Bhooshan Patil <patil.bhooshan@outlook.com>
2021-10-02 14:49:09 +0530

    Update ViewController.swift

--- jenkins-demo-app/jenkins-demo-app/ViewController.swift
+++ jenkins-demo-app/jenkins-demo-app/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    print(#function, "#7")
+    print(#function, "#8")
   }
 
 
